856d3aed28679290a990c33c8856d260d28919de
robrons96 <robin_ronson@yahoo.com>
2017-10-31 02:01:35 -0500

    function for prediction for diffrent layer depth

--- classifier.py
+++ classifier.py
@@ -173,3 +173,17 @@ parameters = nn_model(X, Y, n_h = 4, num_iterations = 10000, print_cost=True)
 
 #building the prediction for the model. 
 plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
+
+
+#perdiction for neural nets with diffent layers. 
+
+plt.figure(figsize=(16, 32))
+hidden_layer_sizes = [1, 2, 3, 4, 5, 20, 50]
+for i, n_h in enumerate(hidden_layer_sizes):
+    plt.subplot(5, 2, i+1)
+    plt.title('Hidden Layer of size %d' % n_h)
+    parameters = nn_model(X, Y, n_h, num_iterations = 5000)
+    plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
+    predictions = predict(parameters, X)
+    accuracy = float((np.dot(Y,predictions.T) + np.dot(1-Y,1-predictions.T))/float(Y.size)*100)
+    print ("Accuracy for {} hidden units: {} %".format(n_h, accuracy))
