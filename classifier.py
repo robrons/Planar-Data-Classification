a8207f301e7741301f45f7d9a9cdae8cc523d2ee
robrons96 <robin_ronson@yahoo.com>
2017-10-31 01:56:38 -0500

    defined final model

--- classifier.py
+++ classifier.py
@@ -127,3 +127,36 @@ def update_parameters(parameters, grads, learning_rate = 1.2):
                   "b2": b2}
     
     return parameters
+
+
+def nn_model(X, Y, n_h, num_iterations = 10000, print_cost=False):
+   
+    
+    np.random.seed(3)
+    n_x = layer_sizes(X, Y)[0]
+    n_y = layer_sizes(X, Y)[2]
+
+    parameters = initialize_parameters(n_x, n_h, n_y)
+    W1 = parameters["W1"]
+    b1 = parameters["b1"]
+    W2 = parameters["W2"]
+    b2 = parameters["b2"]
+   
+    for i in range(0, num_iterations):
+         
+     
+        A2, cache = forward_propagation(X, parameters)
+        
+      
+        cost = compute_cost(A2, Y, parameters)
+ 
+       
+        grads = backward_propagation(parameters, cache, X, Y)
+ 
+        parameters = update_parameters(parameters, grads)
+        
+        
+        if print_cost and i % 1000 == 0:
+            print ("Cost after iteration %i: %f" %(i, cost))
+
+    return parameters
