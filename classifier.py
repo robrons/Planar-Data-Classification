9e1401288b157cd1857eedb42a60b14121d955ed
robrons96 <robin_ronson@yahoo.com>
2017-10-31 01:53:32 -0500

    defined back propagation

--- classifier.py
+++ classifier.py
@@ -77,3 +77,28 @@ def compute_cost(A2, Y, parameters):
     assert(isinstance(cost, float))
     
     return cost
+
+ def backward_propagation(parameters, cache, X, Y):
+   
+    m = X.shape[1]
+    
+   
+    W1 = parameters["W1"]
+    W2 = parameters["W2"]
+  
+    A1 = cache["A1"]
+    A2 = cache["A2"]
+   
+    dZ2 = A2 - Y
+    dW2 = (1/m)*np.dot(dZ2, A1.T)
+    db2 = (1/m)*np.sum(dZ2, axis = 1, keepdims = True)
+    dZ1 = np.multiply(np.dot(W2.T, dZ2), (1 - np.power(A1, 2)))
+    dW1 = (1/m)*np.dot(dZ1, X.T)
+    db1 = (1/m)*np.sum(dZ1, axis = 1, keepdims = True)
+    
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+    
+    return grads
