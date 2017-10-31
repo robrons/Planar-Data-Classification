d2fb4384b5f099c88d8dca9c8803e702323f7fdc
robrons96 <robin_ronson@yahoo.com>
2017-10-31 01:52:03 -0500

    added cost funtion

--- classifier.py
+++ classifier.py
@@ -44,30 +44,16 @@ def initialize_parameters(n_x, n_h, n_y):
 
 
 def forward_propagation(X, parameters):
-    """
-    Argument:
-    X -- input data of size (n_x, m)
-    parameters -- python dictionary containing your parameters (output of initialization function)
-    
-    Returns:
-    A2 -- The sigmoid output of the second activation
-    cache -- a dictionary containing "Z1", "A1", "Z2" and "A2"
-    """
-    # Retrieve each parameter from the dictionary "parameters"
-    ### START CODE HERE ### (≈ 4 lines of code)
+   
     W1 = parameters["W1"]
     b1 = parameters["b1"]
     W2 = parameters["W2"]
     b2 = parameters["b2"]
-    ### END CODE HERE ###
     
-    # Implement Forward Propagation to calculate A2 (probabilities)
-    ### START CODE HERE ### (≈ 4 lines of code)
     Z1 = np.dot(W1, X) + b1
     A1 = np.tanh(Z1)
     Z2 = np.dot(W2, A1) + b2
     A2 = sigmoid(Z2)
-    ### END CODE HERE ###
     
     assert(A2.shape == (1, X.shape[1]))
     
@@ -77,3 +63,17 @@ def forward_propagation(X, parameters):
              "A2": A2}
     
     return A2, cache
+
+
+
+def compute_cost(A2, Y, parameters):
+   
+    m = Y.shape[1]
+
+    logprobs = np.multiply(np.log(A2),Y) + np.multiply(np.log(1-A2), (1-Y))
+    cost = np.sum(logprobs)
+    
+    cost = -np.squeeze(cost)/m     
+    assert(isinstance(cost, float))
+    
+    return cost
